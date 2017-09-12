ee8a7448e8ece462a13ae28b43a5b3ac4a55d1a4
shipy <shipy@shipydeiMac.local>
2017-09-12 18:15:26 +0800

    Loop through the folder to get all the CSV files

--- src/main/java/com/spy/utility/csv/MergerCsvS.java
+++ src/main/java/com/spy/utility/csv/MergerCsvS.java
@@ -1,6 +1,10 @@
 package com.spy.utility.csv;
 
-import com.spy.utility.rw.ReadFile;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.spy.utility.file.TraverseDir;
 
 /**
  * 合并csv文件
@@ -10,14 +14,28 @@ import com.spy.utility.rw.ReadFile;
 public class MergerCsvS {
 
 	public static void main(String[] args) {
-		String path = "/Users/shipy/Desktop/A智能助手/智能助手20170908/邹晓栋/";
+		// String path = "/Users/shipy/Desktop/A智能助手/智能助手20170908/邹晓栋/";
+		String path = "/Users/shipy/Desktop/A智能助手/智能助手20170908";
 		String fName = "ZNZSSCM_JTQSG--今日强势股.csv";
 
-		StringBuffer sb = new StringBuffer();
-		ReadFile.read1(path + fName, sb);
+		// StringBuffer sb = new StringBuffer();
+		// ReadFile.read1(path + fName, sb);
+		//
+		// String str = sb.toString();
+		// System.out.println(str);
+		// System.out.println("===================");
+		// int indexOf = str.indexOf("\n");
+		// String substring = str.substring(indexOf);
+		// System.out.println(substring);
+		// System.out.println("===================");
 
-		String str = sb.toString();
-		System.out.println(sb.toString());
-		str.indexOf("\n");
+		List<File> list = new ArrayList<File>();
+		File folder = new File(path);
+		if (folder.exists()) {
+			TraverseDir.getDirectory(folder, "csv", list);
+		}
+		for (File file : list) {
+			System.out.println(file.getName());
+		}
 	}
 }
--- src/main/java/com/spy/utility/file/TraverseDir.java
+++ src/main/java/com/spy/utility/file/TraverseDir.java
@@ -1,6 +1,8 @@
 package com.spy.utility.file;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 遍历文件夹
@@ -15,7 +17,7 @@ public class TraverseDir {
 	}
 
 	// 递归遍历
-	private static void getDirectory(File file) {
+	public static void getDirectory(File file) {
 		File flist[] = file.listFiles();
 		if (flist == null || flist.length == 0) {
 			return;
@@ -28,6 +30,39 @@ public class TraverseDir {
 			} else {
 				// 这里将列出所有的文件
 				System.out.println("file==>" + f.getAbsolutePath());
+			}
+		}
+	}
+
+	/**
+	 * 递归遍历(提取相应类型的文件)
+	 * 
+	 * @param file
+	 *            初始文件夹
+	 * @param suf
+	 *            文件后缀名
+	 */
+	public static void getDirectory(File file, String suf, List<File> list) {
+		File flist[] = file.listFiles();
+		if (flist == null || flist.length == 0) {
+			return;
+		}
+		for (File f : flist) {
+			if (f.isDirectory()) {
+				// 这里将列出所有的文件夹
+				// System.out.println("Dir==>" + f.getAbsolutePath());
+				getDirectory(f, suf, list);
+			} else {
+				// 这里将列出所有的文件
+				// System.out.println("file==>" + f.getAbsolutePath());
+				String name = f.getName();
+				String[] strs = name.split("\\.");
+				if (suf.equalsIgnoreCase(strs[strs.length - 1])) {
+					if (list == null) {
+						list = new ArrayList<File>();
+					}
+					list.add(f);
+				}
 			}
 		}
 	}
